d2913129bef9e7c1cf42ca42950e51aee296f7ff
Emily Sherwood <emily.sherwood@loucoll.ac.uk>
2026-06-22 15:02:53 +0100

    Starting calendar component

---
+++ src/atoms/H2.jsx
@@ -0,0 +1,5 @@
+export default function H2({text}) {
+    return (
+        <h2 className="text-emerald-700 text-xl font-bold mb-2">{text}</h2>
+    )
+}
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,13 +1,35 @@
+import H2 from "../atoms/H2";
+
 export default function Calendar() {
     return (
-        <div className="mt-6 p-4 border-3 border-dashed border-emerald-300">
-            <p>Monday</p>
-            <p>Tuesday</p>
-            <p>Wednesday</p>
-            <p>Thursday</p>
-            <p>Friday</p>
-            <p>Saturday</p>
-            <p>Sunday</p>
+        <div className="mt-6 p-4 border-y-3 border-dashed border-emerald-300">
+            <H2 text="Meals coming up"></H2>
+
+            <div className="grid grid-cols-2">
+
+                <div>
+                    <p>Monday</p>
+                    <p>Tuesday</p>
+                    <p>Wednesday</p>
+                    <p>Thursday</p>
+                    <p>Friday</p>
+                    <p>Saturday</p>
+                    <p>Sunday</p>
+                </div>
+
+                <div>
+                    <p>Meal</p>
+                    <p>Meal</p>
+                    <p>Meal</p>
+                    <p>Meal</p>
+                    <p>Meal</p>
+                    <p>Meal</p>
+                    <p>Meal</p>
+                </div>
+
+            </div>
+
+            
         </div>
     )
 }

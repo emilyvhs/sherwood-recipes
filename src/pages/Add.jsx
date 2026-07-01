ff629cdbf41282d041c068386dc0185eecdf6db4
Emily Sherwood <emily.sherwood@loucoll.ac.uk>
2026-07-01 14:13:47 +0100

    Add form submits new recipe to database

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,64 +1,143 @@
+import { useState } from "react";
 import Header from "../components/Header";
 
+
+
 export default function Add() {
+
+    const [name, setName] = useState("");
+    const [portions, setPortions] = useState(0);
+    const [ingredients, setIngredients] = useState([]);
+    const [chefNames, setChefNames] = useState([]);
+    const [recipeLocation, setRecipeLocation] = useState("");
+    const [lastCooked, setLastCooked] = useState();
+
+    function addIngredient(e){
+        e.preventDefault();
+        
+        let ingredientValue = document.getElementById('ingredient').value.trim();
+        let quantityValue = document.getElementById('quantity').value.trim();
+        let error = document.getElementById('ingredient-quantity-error');
+
+        if(!error.classList.contains("hidden")) {
+            error.classList.add("hidden");
+        };
+
+        if(!ingredientValue || !quantityValue || ingredientValue === "" || quantityValue === "") {
+            error.classList.remove("hidden");
+        } else {
+            setIngredients([
+                ...ingredients,
+                { ingredient: ingredientValue, quantity: quantityValue }
+            ]);            
+
+            document.getElementById('ingredient').value = "";
+            document.getElementById('quantity').value = "";
+        };
+    }; 
+
+    function addRecipe() {
+
+        const data = {
+            "name": name,
+            "portions": portions,
+            "ingredients": ingredients,
+            "recipeLocation": recipeLocation,
+            "lastCooked": lastCooked
+        };
+
+        const requestOptions = {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(data)
+        };
+
+        fetch('http://localhost:3000/api/recipes/add', requestOptions)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.message)
+            })
+    }
+
     return (
         <div>
             <Header></Header>
 
-            <form className="flex flex-col">
+            <form className="flex flex-col" onSubmit={addRecipe}>
                 <label htmlFor="name">Name of recipe</label>
-                <input type="text" name="name" id="name" placeholder="e.g. Bean's Beans" required
+                <input onChange={(e) => {setName(e.target.value)}} 
+                type="text" name="name" id="name" placeholder="e.g. Bean's Beans" required
                 className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-4" />
 
                 <label htmlFor="portions">Portions</label>
-                <input type="number" name="portions" id="portions"
+                <input onChange={(e) => {setPortions(e.target.value)}} 
+                type="number" name="portions" id="portions"
                 className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-4" />
 
                 <div className="grid grid-cols-2 gap-4 mb-1">
 
                     <div className="flex flex-col">
-                        <label htmlFor="ingredients[]">Ingredients</label>
-                        <input type="text" name="ingredients[]" placeholder="e.g. Tin of chopped tomatoes" required
-                        className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
-                        <input type="text" name="ingredients[]" placeholder="e.g. Onion"
-                        className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
-                        <input type="text" name="ingredients[]" placeholder="e.g. Garlic"
-                        className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
-                        <input type="text" name="ingredients[]" placeholder="e.g. Paprika"
-                        className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
-                        <input type="text" name="ingredients[]" placeholder="e.g. Chilli flakes"
+                        <label htmlFor="ingredient">Ingredient</label> 
+                        <input type="text" name="ingredient" id="ingredient" placeholder="e.g. Tomatoes"
                         className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
                     </div>                
 
                     <div className="flex flex-col">
-                        <label htmlFor="quantities[]">Quantities</label>
-                        <input type="text" name="quantities[]" placeholder="e.g. x1" className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
-                        <input type="text" name="quantities[]" placeholder="e.g. 200g" className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
-                        <input type="text" name="quantities[]" placeholder="e.g. 1 teaspoon" className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
-                        <input type="text" name="quantities[]" placeholder="e.g. 3 tablespoons" className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
-                        <input type="text" name="quantities[]" placeholder="e.g. 1 pinch" className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
+                        <label htmlFor="quantity">Quantity</label>
+                        <input type="text" name="quantity" id="quantity" placeholder="e.g. 200g" className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
                     </div>
 
                 </div>
 
                 <div className="flex justify-end mb-4">
-                    <p className="text-right">Add another ingredient</p>
-                    <button className="inline-flex items-center self-center text-rose-500 font-semibold hover:text-rose-700 bg-rose-200 hover:bg-rose-300 px-2 mx-2 rounded-full pb-1 cursor-pointer h-[28px]">+</button>
+                    <p className="text-right">Add ingredient</p>
+                    <button onClick={addIngredient} className="inline-flex items-center self-center text-rose-500 font-semibold hover:text-rose-700 bg-rose-200 hover:bg-rose-300 px-2 mx-2 rounded-full pb-1 cursor-pointer h-[28px]">+</button>
+                </div>
+                <p className="text-right text-rose-800 text-sm hidden mb-2" id="ingredient-quantity-error">Please input both an ingredient and a quantity to add it to the recipe!</p>
+
+                <div className="grid grid-cols-[3fr_3fr_1fr] gap-4 mb-1">
+                    <div className="flex flex-col">                        
+                        {ingredients.map(ingredient => {
+                            return (
+                                <p className="bg-olive-200 rounded-md border-1 border-olive-300 pl-2 text-sm py-1 mb-1">{ingredient.ingredient}</p>
+                            )
+                        })}
+                    </div>  
+                    <div className="flex flex-col">
+                        {ingredients.map(ingredient => {                
+                            return (
+                                <p className="bg-olive-200 rounded-md border-1 border-olive-300 pl-2 text-sm py-1 mb-1">{ingredient.quantity}</p>
+                            )     
+                        })}
+                    </div>
+                    <div className="flex flex-col">
+                        {ingredients.map(ingredient => {
+                            return (
+                                <button onClick={() => {
+                                    setIngredients(
+                                        ingredients.filter(a => 
+                                            a.ingredient !== ingredient.ingredient
+                                        )
+                                    )
+                                }} key={ingredient.ingredient} id={ingredient.ingredient} className="pl-2 text-left py-1 font-bold text-rose-800 text-sm cursor-pointer">Remove</button>
+                            )
+                        })}
+                    </div>
                 </div>
 
-                <label htmlFor="chefNames[]">Who can cook this recipe?</label>
-                <input type="text" name="chefNames[]" placeholder="e.g. Emily" required className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-1" />
-
-                <div className="flex justify-end mb-4">
-                    <p className="text-right">Add another person who can cook this recipe</p>
-                    <button className="inline-flex items-center self-center text-rose-500 font-semibold hover:text-rose-700 bg-rose-200 hover:bg-rose-300 px-2 mx-2 rounded-full pb-1 cursor-pointer h-[28px]">+</button>
-                </div>
+                <p className="mb-4">Who can cook this recipe?</p>
 
                 <label htmlFor="recipeLocation">Where can this recipe be found?</label>
-                <input type="text" name="recipeLocation" id="recipeLocation" placeholder="e.g. a link to a website / the name of a cookbook" required className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-4" />
+                <input onChange={(e) => {setRecipeLocation(e.target.value)}}
+                type="text" name="recipeLocation" id="recipeLocation" placeholder="e.g. a link to a website / the name of a cookbook" required className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-4" />
 
                 <label htmlFor="lastCooked">When did you last cook this recipe?</label>
-                <input type="date" name="lastCooked" id="lastCooked" required className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-4" />
+                <input onChange={(e) => {setLastCooked(e.target.value)}} 
+                type="date" name="lastCooked" id="lastCooked" required className="bg-white rounded-md border-1 border-rose-100 pl-2 py-1 shadow-sm shadow-olive-300 focus:outline focus:outline-rose-300 mb-4" />
 
                 <input type="submit" value="Add new recipe" className="text-rose-500 font-semibold hover:text-rose-700 bg-rose-200 hover:bg-rose-300 px-2 mx-2 mt-4 rounded-full pb-1 cursor-pointer" />
 

9ca4ae72920b7cda6a2dff2cf806c177dbb6a380
Emily Sherwood <emily.sherwood@loucoll.ac.uk>
2026-07-07 08:30:14 +0100

    Adding sorting to Recipes page

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -5,17 +5,33 @@ import { useEffect, useState } from "react";
 
 export default function Recipes() {
 
+    const [recipesData, setRecipesData] = useState([]);
     const [recipes, setRecipes] = useState([]);
 
     function getRecipes() {
         fetch(`${process.env.HOST}/api/recipes`)
             .then(res => res.json())
-            .then(recipes => {
-                setRecipes(recipes.data);                
+            .then(recipes => {   
+                setRecipesData(recipes.data);
             });
     };
 
+    function sortRecipesByLastCooked() {
+        const sorted = [...recipesData].sort((a, b) => {
+            return b.lastCooked.localeCompare(a.lastCooked)
+        });
+        setRecipes(sorted);
+    };
+
+    function sortRecipesByName() {
+        const sorted = [...recipesData].sort((a, b) => {
+            return a.name.localeCompare(b.name)
+        });
+        setRecipes(sorted);
+    };
+
     useEffect(getRecipes, []);
+    useEffect(sortRecipesByLastCooked, [recipesData]);
 
     return (
         <div>
@@ -26,24 +42,26 @@ export default function Recipes() {
                 <Link to="/add" className="inline-flex items-center text-rose-500 font-semibold hover:text-rose-700 bg-rose-200 hover:bg-rose-300 px-2 mx-2 rounded-full pb-1">+</Link>
             </section>
 
-            <section className="p-4 border-y-3 border-dashed border-emerald-300">
-       
-                
+            <section className="p-4 border-y-3 border-dashed border-emerald-300">  
 
                 <div className="grid grid-cols-2">
                     <div>
-                        <H2 text="Recipe"></H2>
+                        <button onClick={sortRecipesByName}>
+                            <H2 text="Recipe"></H2>
+                        </button>                        
                         {recipes.map(recipe => {
                             return (
-                                <Link key={recipe._id} to={`/recipes/${recipe._id}`}><p>{recipe.name}</p></Link>
+                                <Link key={recipe._id} to={`/recipes/${recipe._id}`} className="underline underline-offset-2 hover:decoration-wavy text-rose-700 hover:text-rose-500"><p>{recipe.name}</p></Link>
                             )
                         })}
                     </div>
                     <div>
-                        <H2 text="Last cooked"></H2>
+                        <button onClick={sortRecipesByLastCooked}>
+                            <H2 text="Last cooked" ></H2>
+                        </button>
                         {recipes.map(recipe => {
                             return (
-                                <p>{recipe.lastCooked.split('T')[0]}</p>
+                                <p key={recipe._id}>{recipe.lastCooked.split('T')[0]}</p>
                             )
                         })}                       
                     </div>

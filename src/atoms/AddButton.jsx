16c6a09002bc2e644dd9299ed72d11e4e5478e3e
Emily Sherwood <emily.sherwood@loucoll.ac.uk>
2026-07-15 10:22:16 +0100

    Creating AddButton component

---
+++ src/atoms/AddButton.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+export default function AddButton({link, linkTo, onClick, value}) {
+
+    if (link === true) {
+
+        return (
+            <Link to={linkTo} 
+            className="flex justify-center 
+            text-rose-500 hover:text-rose-700 font-bold 
+            bg-rose-200 hover:bg-rose-300 
+            w-[35px] h-[35px] text-[20px] leading-[34px]
+            rounded-full">
+                +
+            </Link>
+        )
+
+    } else {
+
+        return (
+            <button onClick={onClick} value={value} 
+            className="flex justify-center 
+            text-rose-500 hover:text-rose-700 font-bold 
+            bg-rose-200 hover:bg-rose-300 
+            w-[35px] h-[35px] text-[20px] leading-[34px]
+            rounded-full">
+                +
+            </button>
+        )
+
+    };
+};
